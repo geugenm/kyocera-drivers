951003c602151d2ad0344b1f34d25d6a5dc6dba0
evgeniy.gleba <glebajk@gmail.com>
2024-06-25 12:33:16 +0300

    Repo upgraded, added files from template cpp repo

---
+++ src/halfton.h
@@ -0,0 +1,20 @@
+/*
+ * Kyocera KPSL filter for CUPS.
+ *
+ * Copyright 2015 by svolkov
+ *
+ * Licensed under Apache License v2.0.  See the file "LICENSE" for more information.
+ */
+
+//
+// Created by svolkov on 29.09.15.
+//
+
+#ifndef RASTERTOKPSL_HALFTON_H
+#define RASTERTOKPSL_HALFTON_H
+
+#include "stdlib.h"
+
+void HalftoneDibToDib(unsigned char*, unsigned char*, int, int, int, int);
+
+#endif //RASTERTOKPSL_HALFTON_H
